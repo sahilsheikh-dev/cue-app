945270522294e9a4ac4ac4cb2a47aaae61c3a0da
Sahil Sheikh <sheikh9@avaya.com>
2025-09-15 20:52:56 -0400

    env-bug-fix

--- src/config/app.config.jsx
+++ src/config/app.config.jsx
@@ -1,7 +1,3 @@
 // src/config/app.config.js
-import { API_URL, STRIPE_PK } from '@env';
-
-export const BASE_API_URL = API_URL || 'http://localhost:9000';
-export const STRIPE_PUBLISHABLE_KEY =
-  STRIPE_PK ||
-  'pk_test_51QUpeKAgw3asoEkcwZXNQBnVDY99IjwwIEzJZAIKw3iu3FaM2vFzlTObWHVhS3JXXhEAmUXIQSS4NovDy9WiXoLB0067DbJvYP';
+export const BASE_API_URL = process.env.API_URL || "http://localhost:9000";
+export const STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PK || 'pk_test_51QUpeKAgw3asoEkcwZXNQBnVDY99IjwwIEzJZAIKw3iu3FaM2vFzlTObWHVhS3JXXhEAmUXIQSS4NovDy9WiXoLB0067DbJvYP';
