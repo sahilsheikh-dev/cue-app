e7648a71b3d451126e1c64b115bde7d539281fc8
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2025-09-23 12:32:21 +0530

    coach screens added

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-// import { StripeProvider } from "@stripe/stripe-react-native";
+import { StripeProvider } from "@stripe/stripe-react-native";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -95,7 +95,7 @@ export default function App() {
   // ---------- Render ----------
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      {/* <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}> */}
+      <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
         <DataContext.Provider
           value={{
             data,
@@ -118,7 +118,7 @@ export default function App() {
           </NavigationContainer>
         </DataContext.Provider>
         <StatusBar style="auto" />
-      {/* </StripeProvider> */}
+      </StripeProvider>
     </GestureHandlerRootView>
   );
 }
